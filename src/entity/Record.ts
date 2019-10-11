ce01086fe0295066eb3b42b6225a4eaab678655e
Lewis <lewisfmcn@yahoo.co.uk>
2019-10-11 23:31:08 +0100

    add Record class

---
+++ src/entity/Record.ts
@@ -0,0 +1,24 @@
+import JiraIssue from "./JiraIssue";
+import Worklog from "./Worklog"
+
+export default class Record {
+    private readonly jiraIssue: JiraIssue;
+    private readonly worklogs: Worklog[];
+
+    constructor(jiraIssue: JiraIssue) {
+        this.jiraIssue = jiraIssue;
+        this.worklogs = [];
+    }
+
+    public addWorklog(worklog: Worklog) {
+        this.worklogs.push(worklog);
+    }
+
+    public getJiraIssue(): JiraIssue {
+        return this.jiraIssue;
+    }
+
+    public getWorklogs(): Worklog[] {
+        return this.worklogs;
+    }
+}
